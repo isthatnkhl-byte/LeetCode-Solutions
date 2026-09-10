a38c3823daf66c83b2497f948d3f5f5dc9a66f8c
isthatnkhl-byte <nikhilggoswami5380@gmail.com>
2026-09-10 06:42:57 +0530

    Add solution for cpp-sandbox

--- cpp-sandbox/cpp-sandbox.cpp
+++ cpp-sandbox/cpp-sandbox.cpp
@@ -1,38 +1,29 @@
 #include <iostream>
+#include <unordered_set>
 #include <algorithm>
-#include <vector>
+#include <climits>
 using namespace std;
-//better approach for rotate matrix 90.
-//step 1: transpose the matrix (interchange row and column)
-//step 2: reverse
-void rotate90(vector<vector<int>>& matrix, int& n) {
-    //transposing 
-    for (int i = 0; i < n-1; i++) {
-        for (int j = i+1; j < n; j++) {
-              swap(matrix[i][j], matrix[j][i]);
-            }
-        }
-      for (int k = 0; k < n; k++) {
-           reverse(matrix[k].begin(), matrix[k].end());
+//step 1 : traverse through the array 
+//step 2 : add current element into the value of sum (initially 0)
+//step 2 : if current value of sum is greater than max ( max = INT_MIN ) replace max with sum
+//step 3 : if sum value currently is negative set it back to 0
+//step 4 : if sum value currently is positive add the next array value into it
+int kadanealgo(int arr[], int& n) {
+    int maxi = INT_MIN;
+    int sum = 0;
+      for (int i = 0; i < n; i++) {
+             sum+=arr[i];
+             maxi = max(maxi,sum);
+             if (sum < 0 ) {
+                  sum = 0;
+                 }
           }
-    }
-
-
+       return maxi;
+}
+    
 int main() {
-    vector<vector<int>> matrix = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
-    int n = matrix.size();
-    rotate90(matrix, n);
-    
-    for (const auto& row : matrix) {
-        for (int val : row) {
-            std::cout << val << " ";
-        }
-        std::cout << "\n";
-    }
-    
+    int arr[] = {-10, -2, 100, -101, 102, 103, -3, 4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << "max subarray sum = " << kadanealgo(arr,n);
     return 0;
     }
